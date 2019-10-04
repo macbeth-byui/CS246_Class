9c95205829b013236260941d349b1515cef70c90
macbeth-byui <31939230+macbeth-byui@users.noreply.github.com>
2019-10-04 15:08:45 -0600

    added json and sorting

--- EarthquakeAnalyzer/src/main/java/macbeth/EarthquakeAnalyzer.java
+++ EarthquakeAnalyzer/src/main/java/macbeth/EarthquakeAnalyzer.java
@@ -44,7 +44,38 @@ public class EarthquakeAnalyzer {
     }
 
     public void displayEarthquakeData() {
+        String data = loadWebData("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson");
+        Gson gson = new Gson();
+        EarthquakeList eql = gson.fromJson(data, EarthquakeList.class);
+        // The Collections.sort function is a very useful tool to sort a list.  If the list was just numbers
+        // or Strings, then we could just pass the list in and it would sort.  However, to sort the list in the
+        // EarthquakeList class, we will need to tell Java how to compare 2 EarthquakeEvent's. By implementing a
+        // Comparator object we can tell it how to compare.  Notice that Comparator is an Interface.  To create an
+        // object of an interface, we have to supply a definition for the abstract function within it.  We call
+        // this an anonymous class since we have the function implementation without giving a name to the class.
 
+        /*Collections.sort(eql.getEarthquakes(), new Comparator<EarthquakeEvent>() {
+            @Override
+            public int compare(EarthquakeEvent o1, EarthquakeEvent o2) {
+                // We are going to define earthquake comparisons by comparing their magnitudes.
+                // Note that this function needs to return an integer that describes the difference between
+                // the 2 objects.  We will use the compare function provided in the Float class to compare
+                // the 2 magnitudes.
+                return Float.compare(o1.getDetail().getMag(), o2.getDetail().getMag());
+            }
+        });*/
+
+        // There is another way we can do this.  We can create a lambda function (a function that has no name) that
+        // will be interpreted as the compare function.  Our function still needs 2 parameters (o1 and o2) just like
+        // the compare function did above.  We use the "->" to signal that this is a lambda function.  The value
+        // on the right side of the arrow is what will be returned.  Compare this code with the one above.  They
+        // do the same thing.
+
+        Collections.sort(eql.getEarthquakes(), (o1,o2)->Float.compare(o1.getDetail().getMag(), o2.getDetail().getMag()));
+
+        for (EarthquakeEvent e : eql.getEarthquakes()) {
+            System.out.println(e);
+        }
 
     }
 
