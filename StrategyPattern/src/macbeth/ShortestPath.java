5305642586b537bc0f3476a676a4c2c301e6255c
macbeth-byui <31939230+macbeth-byui@users.noreply.github.com>
2019-11-04 11:39:29 -0700

    Strategy Example

---
+++ StrategyPattern/src/macbeth/ShortestPath.java
@@ -0,0 +1,11 @@
+package macbeth;
+
+import java.util.List;
+
+public class ShortestPath implements Paths  {
+    @Override
+    public List<String> findRoute(String start, String end) {
+        System.out.println("Finding Shortest Path");
+        return null;
+    }
+}
