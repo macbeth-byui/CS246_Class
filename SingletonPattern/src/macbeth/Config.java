3fb5a38a9c83a15b3793fafc40e3073a16453963
macbeth-byui <31939230+macbeth-byui@users.noreply.github.com>
2019-11-25 08:18:07 -0700

    singleton example

---
+++ SingletonPattern/src/macbeth/Config.java
@@ -0,0 +1,57 @@
+package macbeth;
+
+import java.io.*;
+import java.util.TreeMap;
+
+public class Config {
+    private TreeMap<String, String> data;
+
+    public Config() {
+        data = new TreeMap<String, String>();
+
+        readFile();
+    }
+
+    public String getConfigValue(String key) {
+        return data.get(key);
+    }
+
+    public void setConfigValue(String key, String value) {
+        data.put(key, value);
+
+        writeFile();
+    }
+
+    private void readFile() {
+        // Read from the file to populate the configuration
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("config.txt"));
+            String line;
+            do {
+                line = reader.readLine();
+                if (line != null) {
+                    String[] result = line.split("=");
+                    data.put(result[0], result[1]); // TODO: Need to add some more error checking
+                }
+            }
+            while (line != null);
+            reader.close();
+        }
+        catch (IOException ioe) {
+        }
+    }
+
+    private void writeFile() {
+
+        // Write to the file immediately to update the configuration
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("config.txt"));
+            for (String dataKey : data.keySet()) {
+                writer.write(dataKey+"="+data.get(dataKey)+"\n");
+            }
+            writer.close();
+        }
+        catch (IOException ioe) {
+        }
+    }
+}
