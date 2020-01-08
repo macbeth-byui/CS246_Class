3780f9ac30e281755fb4716b5b883072b73fd0fd
macbethc-byui <macbethc@byui.edu>
2020-01-08 13:05:28 -0700

    First Example

--- HelloWorld/src/macbeth/Hello.java
+++ HelloWorld/src/macbeth/Hello.java
@@ -1,11 +1,12 @@
 package macbeth;
 
 public class Hello {
+
     public void sayHello(String name) {
-        System.out.println("Hello "+name+"!");
+        System.out.println("Hello, " + name);
     }
 
-    public static void main(String[] args) {
+    public static void main(String [] args) {
         Hello hello = new Hello();
         hello.sayHello("Bob");
     }
