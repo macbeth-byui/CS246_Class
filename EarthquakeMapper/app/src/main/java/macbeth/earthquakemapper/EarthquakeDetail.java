63302d5672acf1ccf9241c6ab4563820202fe15c
macbethc-byui <macbethc@byui.edu>
2020-05-27 15:03:55 -0600

    Add new examples of threading

---
+++ EarthquakeMapper/app/src/main/java/macbeth/earthquakemapper/EarthquakeDetail.java
@@ -0,0 +1,16 @@
+package macbeth.earthquakemapper;
+
+/**
+ * Contains the detailed earthquake information from the USGS Earthquake JSON
+ *
+ * https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
+ */
+public class EarthquakeDetail {
+
+    private float mag;
+
+    public float getMag() {
+        return mag;
+    }
+
+}
