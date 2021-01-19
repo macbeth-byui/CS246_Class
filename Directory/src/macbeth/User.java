bc5c84275e18738fdf8cac21d645db2253968752
Chad Macbeth <macbethc@byui.edu>
2021-01-19 08:02:52 -0700

    Data Structures and JSON

---
+++ Directory/src/macbeth/User.java
@@ -0,0 +1,22 @@
+package macbeth;
+
+public class User {
+    private String name;
+    private int age;
+    private String phone;
+
+    public User(String name, int age, String phone) {
+        this.name = name;
+        this.age = age;
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+}
